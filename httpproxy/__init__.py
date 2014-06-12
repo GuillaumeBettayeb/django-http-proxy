85c536893a4751b0245fca0440d888b3f3b168bf
Colin Barnwell <cbarnwell@twig-it.com>
2014-06-12 15:36:36 +0100

    New version number

--- httpproxy/__init__.py
+++ httpproxy/__init__.py
@@ -2,5 +2,5 @@
 Django HTTP Proxy - A simple HTTP proxy for the Django framework.
 """
 
-__version__ = '0.3.3-yola'
+__version__ = '0.4.0'
 __authors__ = ['Yuri van der Meer <django-http-proxy@yvandermeer.net>']
